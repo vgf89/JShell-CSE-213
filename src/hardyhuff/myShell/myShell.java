0ab64df4fabdce31e69c95e72cc205a629597e38
Kevin Hardy <hardy.kevin.c@gmail.com>
2015-04-25 15:52:00 -0600

    Implemented cp and rm

--- src/hardyhuff/myShell/myShell.java
+++ src/hardyhuff/myShell/myShell.java
@@ -5,19 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class myShell {
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		String s;
-		//String command;
 		String[] arguments;
 		
 		while(true) {
 			System.out.print(System.getProperty("user.dir") + "$ ");
-			s = input.readLine();
-			arguments = s.split(" ");
+			arguments = input.readLine().split(" ");
 			switch(arguments[0])
 			{
 			case "cd":
@@ -27,13 +25,13 @@ public class myShell {
 				System.out.println(ls());
 				break;
 			case "cp":
-				System.out.println(arguments[0]);
+				System.out.println(cp(arguments[1], arguments[2]));
 				break;
 			case "mv":
 				System.out.println(arguments[0]);
 				break;
 			case "rm":
-				System.out.println(arguments[0]);
+				System.out.println(rm(arguments[1]));
 				break;
 			case "diff":
 				System.out.println(arguments[0]);
@@ -97,6 +95,7 @@ public class myShell {
 		}
 		return "Directory " + arg + " does not exist";
 	}
+	
 	static String ls() {
 		String s="";
 		File workdir =new File(System.getProperty("user.dir"));
@@ -111,40 +110,103 @@ public class myShell {
 		
 		return s;
 	}
+	
 	static String cp(String arg1, String arg2) {
-		return null;
+		File in = new File(arg1);
+		if (in.isFile()) {
+			File out = new File(arg2);
+			try {
+				Files.copy(in.toPath(), out.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return "Failed to copy file " + arg1 + " to " + arg2;
+			}
+			return "";
+		}
+		
+		in = new File(System.getProperty("user.dir") + "/" + arg1);
+		if (in.isFile()) {
+			File out = new File(arg2);
+			try {
+				Files.copy(in.toPath(), out.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return "Failed to copy file " + arg1 + " to " + arg2;
+			}
+			return "";
+		}
+		
+		return "File " + arg1 + " not found";
 	}
+	
 	static String mv(String arg1, String arg2) {
 		
 		return null;
 	}
+	
 	static String rm(String arg) {
-		return null;
+		File file = new File(arg);
+		if (file.isFile()) {
+			try {
+				Files.delete(file.toPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return "Failed to remove file " + arg;
+			}
+		}
+		
+		file = new File(System.getProperty("user.dir") + "/" + arg);
+		if (file.isFile()) {
+			try {
+				Files.delete(file.toPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return "Failed to remove file " + arg;
+			}
+			return "";
+		}
+		
+		return "Not such file " + arg;
 	}
+	
 	static String diff (String arg1, String arg2) {
 		return null;
 	}
-	static String more(String arg) {
+	
+	//TODO: figure this out for GUI
+	static String more(String arg, int width, int height) {
 		return null;
 	}
+	
 	static String wc(String arg) {
 		return null;
 	}
+	
 	static String mkdir(String arg) {
+		File file = new File(arg);
 		return null;
 	}
+	
 	static String grep(String[] args) {
 		return null;
 	}
+	
 	static String ps() {
 		return null;
 	}
+	
 	static String kill(String arg) {
 		return null;
 	}
+	
 	static String whoami(String arg) {
 		return null;
 	}
+	
 	static String env() {
 		return null;
 	}
